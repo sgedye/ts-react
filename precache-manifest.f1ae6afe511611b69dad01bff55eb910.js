248191c009b54d4381496ec9a82b71fc277aea7d
Shaun Gedye <sgedye88@yahoo.com.au>
2020-05-06 15:18:50 +0800

    Updates

---
+++ precache-manifest.f1ae6afe511611b69dad01bff55eb910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d048a4c2b18ac7d9ada9f054696f030",
+    "url": "/ts-react/index.html"
+  },
+  {
+    "revision": "02cca7f36d02aba68771",
+    "url": "/ts-react/static/css/main.49f83174.chunk.css"
+  },
+  {
+    "revision": "f584cbd6a85fce95f3ba",
+    "url": "/ts-react/static/js/2.bb948e1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ts-react/static/js/2.bb948e1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02cca7f36d02aba68771",
+    "url": "/ts-react/static/js/main.77fd45d4.chunk.js"
+  },
+  {
+    "revision": "a186c718076e88de630e",
+    "url": "/ts-react/static/js/runtime-main.804fc133.js"
+  }
+]);
